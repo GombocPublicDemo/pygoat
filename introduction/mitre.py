439ddb2a4d8be2856fbaec3eeee04aa079fd5524
pepegc <41898282+github-actions[bot]@users.noreply.github.com>
2026-06-18 11:25:20 +0000

    fix(gomboc): gomboc-ai/python/set-cookie/cwe-732-httponly-secure-flags003 (views.py, mitre.py)

--- introduction/mitre.py
+++ introduction/mitre.py
@@ -168,7 +168,7 @@ def csrf_lab_login(request):
             }
             cookie = jwt.encode(payload, 'csrf_vulneribility', algorithm='HS256')
             response = redirect("/mitre/9/lab/transaction")
-            response.set_cookie('auth_cookiee', cookie, httponly=True)
+            response.set_cookie('auth_cookiee', cookie, httponly=True, secure=True)
             return response
         else :
             return redirect('/mitre/9/lab/login')
