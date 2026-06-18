a580f82272049925b12bf1cd0234bb3a62f877b7
pepegc <41898282+github-actions[bot]@users.noreply.github.com>
2026-06-18 11:46:15 +0000

    fix(gomboc): gomboc-ai/python/stdlib/cwe-611-xxe-replace-xml-parsers-with-defusedxml010 (views.py)

--- introduction/views.py
+++ introduction/views.py
@@ -15,7 +15,7 @@ from hashlib import md5
 from io import BytesIO
 from random import randint
 from xml.dom.pulldom import START_ELEMENT, parseString
-from xml.sax import make_parser
+from defusedxml.sax import make_parser
 from xml.sax.handler import feature_external_ges
 
 import jwt
