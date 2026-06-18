4c870e1c1a295f5392a9ecee0997d36118d71733
pepegc <41898282+github-actions[bot]@users.noreply.github.com>
2026-06-18 11:46:15 +0000

    fix(gomboc): gomboc-ai/python/set-cookie/cwe-732-httponly-secure-flags001 (views.py)

--- introduction/views.py
+++ introduction/views.py
@@ -290,7 +290,7 @@ def auth_lab_signup(request):
             try:
                 rendered = render_to_string('Lab/AUTH/auth_success.html', {'username': obj.username,'userid':obj.userid,'name':obj.name,'err_msg':'Cookie Set'})
                 response = HttpResponse(rendered)
-                response.set_cookie('userid', obj.userid, max_age=31449600, samesite=None, secure=False)
+                response.set_cookie('userid', obj.userid, max_age=31449600, samesite=None, secure=True)
                 print('Setting cookie successful')
                 return response
             except:
@@ -304,7 +304,7 @@ def auth_lab_login(request):
             obj = authLogin.objects.filter(userid=request.COOKIES['userid'])[0]
             rendered = render_to_string('Lab/AUTH/auth_success.html', {'username': obj.username,'userid':obj.userid,'name':obj.name, 'err_msg':'Login Successful'})
             response = HttpResponse(rendered)
-            response.set_cookie('userid', obj.userid, max_age=31449600, samesite=None, secure=False)
+            response.set_cookie('userid', obj.userid, max_age=31449600, samesite=None, secure=True)
             print('Login successful')
             return response
         except:
@@ -318,7 +318,7 @@ def auth_lab_login(request):
             try:
                 rendered = render_to_string('Lab/AUTH/auth_success.html', {'username': obj.username,'userid':obj.userid,'name':obj.name, 'err_msg':'Login Successful'})
                 response = HttpResponse(rendered)
-                response.set_cookie('userid', obj.userid, max_age=31449600, samesite=None, secure=False)
+                response.set_cookie('userid', obj.userid, max_age=31449600, samesite=None, secure=True)
                 print('Login successful')
                 return response
             except:
